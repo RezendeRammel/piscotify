40fb13966602674e77f80b1b4e168f4a8b4d13ec
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-04-29 19:32:30 -0300

    feat(album): create albuns image

---
+++ js/album.js
@@ -0,0 +1,44 @@
+const albumData = [
+  {
+    artist: 'The Dark Side of the Moon',
+    album: 'Pink Floyd',
+    date: '1973',
+    link: 'https://open.spotify.com/album/4LH4d3cOWNNsVw41Gqt2kv',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e02f05e5ac32fdd79d100315a20',
+  },
+  {
+    artist: 'Legião Urbana',
+    album: 'Dois',
+    date: '1986',
+    link: 'https://open.spotify.com/album/4OCJTpOLqSgysGY4yBCGhn',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e021eb5e996639036a49b09f1e5',
+  },
+  {
+    artist: 'Linkin Park',
+    album: 'Hybrid Theory Live Around the World',
+    date: '2012',
+    link: 'https://open.spotify.com/album/1FiRqhpAowNK8gTl5sOhxZ',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e028512f90ead4b4412cd7832ae',
+  },
+  {
+    artist: 'O Ritmo Que Conquistou o Brasil',
+    album: 'Banda Calypso',
+    date: '2002',
+    link: 'https://open.spotify.com/album/2SA4KtmfGfLZ5b8BckgCcC',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e029050bae4806a12310d41d8fc',
+  },
+  {
+    artist: 'Californication',
+    album: 'Red Hot Chili Peppers',
+    date: '1999',
+    link: 'https://open.spotify.com/album/0fLhefnjlIV3pGNF9Wo8CD',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e02a9249ebb15ca7a5b75f16a90',
+  },
+  {
+    artist: 'O Furacão É Show',
+    album: 'Banda Djavú',
+    date: '2009',
+    link: 'https://open.spotify.com/album/2MHCQd6vVteNRZz50Z4z3B',
+    linkImg: 'https://i.scdn.co/image/ab67616d00001e02a15289a0674b398b23bd26cd',
+  },
+];
---
+++ js/index.js
@@ -0,0 +1,46 @@
+const albunsElement = document.querySelector('#render-albuns-ul');
+
+albumData.map((item) => {
+  const { artist, album, date, link, linkImg } = item;
+  const albumItemElement = document.createElement('li');
+  albumItemElement.setAttribute('class', 'col');
+
+  const imgAlbumLink = document.createElement('a');
+  imgAlbumLink.setAttribute('href', link);
+  imgAlbumLink.setAttribute('target', 'blank');
+  imgAlbumLink.setAttribute('rel', 'noopener noreferrer');
+
+  const imgElement = document.createElement('img');
+  imgElement.setAttribute('src', linkImg);
+  imgElement.setAttribute('alt', album);
+
+  const artistElement = document.createElement('p');
+  const artistElementText = document.createTextNode(artist);
+  artistElement.appendChild(artistElementText);
+
+  const albumElement = document.createElement('p');
+  const albumElementText = document.createTextNode(album);
+  albumElement.appendChild(albumElementText);
+
+  const dateElement = document.createElement('p');
+  const dateElementText = document.createTextNode(date);
+  dateElement.appendChild(dateElementText);
+
+  imgAlbumLink.appendChild(imgElement);
+
+  albumItemElement.appendChild(imgAlbumLink);
+  albumItemElement.appendChild(artistElement);
+  albumItemElement.appendChild(albumElement);
+  albumItemElement.appendChild(dateElement);
+
+  albunsElement.appendChild(albumItemElement);
+});
+
+{
+  /* <li class="col">
+  <img src="https://i.scdn.co/image/ab67616d00001e021eb5e996639036a49b09f1e5" alt="">
+  <p>Legião Urbana</p>
+  <p>Dois</p>
+  <p>1986</p>
+</li> */
+}
